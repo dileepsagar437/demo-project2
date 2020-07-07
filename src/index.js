b88cc590ecd70860b36623543862605a379b59fa
kdsagar22 <you@example.com>
2020-07-07 10:31:14 +0530

    new changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ***********with jsx*************
+//const element=<h1 className="testclass">hello world</h1>;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//ReactDOM.render(element,document.getElementById("root"));
+
+//const newelement=<h1 className="testclass">iam alone</h1>;
+
+//ReactDOM.render(newelement,document.getElementById("root1"));
+
+/*const element = (
+  <div className='testclass'>
+    <h1>hello</h1>
+    <h2>hello all</h2>
+  </div>
+)
+
+ReactDOM.render(element,document.getElementById("root"));
+*/
+
+//***********with jsx*************
+
+
+
+
+// ***********default*************
+//import './index.css';
+//import App from './App';
+//import * as serviceWorker from './serviceWorker';
+
+//ReactDOM.render(
+  
+  // <React.StrictMode>
+ //<App />
+ //  </React.StrictMode>,
+  // document.getElementById('root')
+   // );
+
+    // If you want your app to work offline and load faster, you can change
+    // unregister() to register() below. Note this comes with some pitfalls.
+   // Learn more about service workers: https://bit.ly/CRA-PWA
+   // serviceWorker.unregister();
+  
